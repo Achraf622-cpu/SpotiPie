dbedbf1774ac8a78b32c8b348acb65f9bfa76ed4
Achraf <hanzazachraf581@gmail.com>
2025-02-04 00:40:12 +0100

    Added the Login methode to the UserModel

--- models/UserModel.php
+++ models/UserModel.php
@@ -6,10 +6,24 @@ class UserModel {
         $this->db = $db;
     }
 
+    // Register a new user
     public function register($username, $email, $password, $role_id) {
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO Users (username, email, password, role_id) VALUES (?, ?, ?, ?)";
         $stmt = $this->db->prepare($query);
         return $stmt->execute([$username, $email, $hashedPassword, $role_id]);
+    }
+
+    // Login a user
+    public function login($username, $password) {
+        $query = "SELECT * FROM Users WHERE username = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute([$username]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            return $user; // Return user data if login is successful
+        }
+        return false; // Return false if login fails
     }
 }
